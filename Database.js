efa9dbe9dc462cde1108358e68014617d1f60678
jacobs67 <jacobs67@wwu.edu>
2020-01-19 11:02:54 -0800

    Moved db to config var

---
+++ Database.js
@@ -0,0 +1,65 @@
+const USE_DB = true;
+
+let mongojs = USE_DB ? require('mongojs') : null;
+let db = USE_DB ? mongojs(process.env.DB_PATH, ['account', 'progress']) : null;
+
+// account: { username: string, password: string }
+// progress: { username: string, items: [{id: string, amount: number }] }
+
+Database = {};
+Database.isValidPassword = (data, cb) => {
+    if(!USE_DB) return cb(true);
+    db.account.findOne({ username: data.username,
+                      password: data.password }, (err, res) => {
+        if(res) {
+            cb(true);
+        } else {
+            cb(false);
+        }
+    });
+}
+
+Database.isUsernameTaken = (data, cb) => {
+    if(!USE_DB) return cb(false);
+    db.account.findOne({ username: data.username }, (err, res) => {
+        if(res) {
+            cb(true);
+        } else {
+            cb(false);
+        }
+    });
+}
+
+Database.addUser = (data, cb) => {
+    if(!USE_DB) return cb();
+    db.account.insert({ username: data.username,
+                        password: data.password }, (err) => {
+        Database.savePlayerProgress({ username: data.username, items: []}, function() {
+            cb();
+        });
+    });
+}
+
+Database.getPlayerProgress = function(username, cb) {
+    if(!USE_DB)
+        return cb({ items: [] });
+    db.progress.findOne({ username: username }, function(err, res) {
+        if(err)
+            console.log(err);
+        else if(!res)
+            return cb({ items: [] })
+        else
+            return cb({ items: res.items });
+    });
+}
+
+Database.savePlayerProgress = function(data, cb) {
+    cb = cb || function() {}
+    if(!USE_DB) return cb();
+    console.log('saving: ' + data.username);
+    db.progress.update({ username: data.username }, { $set: data }, { upsert: true }, function(err, count, status) {
+        console.log('updated ' + count + ' items');
+        console.log('err: ' + err);
+        console.log('status: ' + status);
+    });
+}
